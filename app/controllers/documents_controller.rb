9861363e806b634f6acd04d32eb712f617466a37
Lianna Newman <liannanewman@gmail.com>
2019-04-29 08:42:13 -0400

    Added Document Controller and show, edit, index and new views.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,42 @@
 class DocumentsController < ApplicationController
+  before_action :find_document, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @documents = Document.all
+  end
+
+  def show
+  end
+
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @document = Document.new(document_params)
+    if @document.save
+      render @document
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
+  private
+
+  def find_document
+    @document = Document.find(params[:id])
+  end
+
+  def document_params
+    params.require(:document).permit(:title, :content)
+  end
 end
